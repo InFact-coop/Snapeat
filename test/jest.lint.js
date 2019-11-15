3bbdccf5b73f0e1d2fe95ccea98a5e4e4b371fee
Ivan <ivangonzalez@rocketmail.com>
2019-11-15 12:35:19 +0000

    Add jest boilerplate + jest-runner-eslint for running linting in parallel

---
+++ test/jest.lint.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+const x = {}
+
+module.exports = {
+  rootDir: path.join(__dirname, '..'),
+  displayName: 'lint',
+  runner: 'jest-runner-eslint',
+  testMatch: ['<rootDir>/**/*.js'],
+}
