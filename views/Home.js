37e3fd54e72d6463b60ac52e899ed0e46633f92b
lucymk <lucy.m.king92@gmail.com>
2019-11-19 10:14:30 +0000

    Turns button into file input and saves to state (2)

--- views/Home.js
+++ views/Home.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import { useRouteDispatch } from '../utils/routeContext'
-import { CHANGE_VIEW, MENU, WELCOME } from '../utils/constants'
+import { CHANGE_VIEW, MENU } from '../utils/constants'
+import fileNameFormatter from '../utils/fileNameFormatter'
 
 import buttonBG from '../public/backgrounds/camera_bg.svg'
 import menuBG from '../public/backgrounds/menu_bg.svg'
@@ -14,6 +15,19 @@ import cameraButton from '../public/icons/btn_round.svg'
 
 const Home = () => {
   const routeDispatch = useRouteDispatch()
+  const [photo, setPhoto] = useState({})
+  const onImageSelect = e => {
+    e.preventDefault()
+    const file = e.target.files[0]
+    if (file) {
+      setPhoto({
+        photo: file,
+        photoName: fileNameFormatter(file.name),
+        photoURL: URL.createObjectURL(file),
+      })
+    }
+  }
+
   return (
     <Dashboard>
       <MenuContainer>
@@ -29,19 +43,26 @@ const Home = () => {
         <p className="font-bold">What is your child eating for dinner?</p>
         <p>Share a photo</p>
       </CameraContainer>
-      <ButtonContainer>
-        <p className="self-end">Snap a photo of their meal</p>
-        <div
-          onClick={() => routeDispatch({ type: CHANGE_VIEW, view: WELCOME })}
-          onKeyPress={() => routeDispatch({ type: CHANGE_VIEW, view: WELCOME })}
-        >
+      <ButtonForm>
+        <Label htmlFor="image">
+          <p>Snap a photo of their meal</p>
           <img
             className="w-20 sm:w-auto"
             alt="camera-button"
             src={cameraButton}
           />
-        </div>
-      </ButtonContainer>
+        </Label>
+        <input
+          className="hidden"
+          type="file"
+          id="image"
+          name="image"
+          accept="image/png, image/jpeg"
+          multiple={false}
+          files={photo}
+          onChange={onImageSelect}
+        />
+      </ButtonForm>
     </Dashboard>
   )
 }
@@ -54,13 +75,15 @@ const Dashboard = styled.div.attrs({
   background: url(${cameraBG}) center/45% auto no-repeat;
 `
 
-const ButtonContainer = styled.div.attrs({
-  className: 'w-full',
-})`
+const Label = styled.label`
   display: grid;
   grid-template-rows: 1fr 144px;
   grid-gap: 10px;
   justify-items: center;
+`
+const ButtonForm = styled.form.attrs({
+  className: 'w-full',
+})`
   background: url(${buttonBG}) left bottom/contain no-repeat;
 `
 
