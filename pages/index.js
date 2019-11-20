d164ad94a7a004758b220fbfbe054e746a2300b5
Ivan <ivangonzalez@rocketmail.com>
2019-11-20 23:12:55 +0000

    Add correct metadata for downloading to homescreen

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const Index = () => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="DISCOVERbot" />
+        <meta name="apple-mobile-web-app-title" content="Snapeat" />
         <link href="/manifest.json" rel="manifest" />
         <link href="/iphone-icon.png" rel="apple-touch-icon" sizes="180x180" />
       </Head>
