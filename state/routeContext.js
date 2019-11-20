732d05f41e92c39f999286104ae40fe311c90915
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-11-20 17:39:04 +0000

    Remove extra FOOD_DATA from router context history

--- state/routeContext.js
+++ state/routeContext.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import * as R from 'ramda'
-import { HOME, CHANGE_VIEW, GO_BACK, FOOD_DATA } from '../utils/constants'
+import { HOME, CHANGE_VIEW, GO_BACK } from '../utils/constants'
 
 const initialState = {
   currentView: HOME,
-  history: [HOME, FOOD_DATA],
+  history: [HOME],
 }
 
 const RouteStateContext = React.createContext()
