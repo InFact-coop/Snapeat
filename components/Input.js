df13532c4d8d2dadee4ed751cb3b20f26db11b3d
Max Gerber <mgerber94@gmail.com>
2019-11-25 18:49:13 +0000

    + Dynamic Ages Question
    #16

--- components/Input.js
+++ components/Input.js
@@ -9,11 +9,22 @@ const TextInput = styled(Field).attrs(({ placeholder, name, ...attrs }) => ({
   ...attrs,
 }))``
 
-const RadioInput = styled(Field).attrs(({ placeholder, name, ...attrs }) => ({
-  className: 'bg-lightgray w-full border-b-2 border-navy',
-  type: 'text',
+const RadioLabel = styled.label.attrs({
+  className: 'ml-2d5',
+})``
+
+const Radio = styled(Field).attrs(({ name, ...attrs }) => ({
+  type: 'radio',
+  id: name,
   name,
-  placeholder,
   ...attrs,
 }))``
+
+const RadioInput = ({ name, children, id, ...attrs }) => (
+  <div className="mb-2d5">
+    <Radio name={name} id={id} {...attrs} />
+    <RadioLabel htmlFor={id}>{children}</RadioLabel>
+  </div>
+)
+
 export { TextInput, RadioInput }
