9fb8a26d4a9ee813a66982e80b2bba1bacd71011
Max Gerber <mgerber94@gmail.com>
2019-11-26 15:42:46 +0000

    + Create project context #1

---
+++ state/projectContext.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import { NO_PROJECT, CHANGE_PROJECT } from '../utils/constants'
+
+const initialState = { error: NO_PROJECT }
+
+const ProjectStateContext = React.createContext()
+const ProjectDispatchContext = React.createContext()
+
+const projectReducer = (_, { project, type }) => {
+  switch (type) {
+    case CHANGE_PROJECT:
+      return project
+    default:
+      return { error: NO_PROJECT }
+  }
+}
+
+const ProjectProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(projectReducer, initialState)
+  return (
+    <ProjectStateContext.Provider value={state}>
+      <ProjectDispatchContext.Provider value={dispatch}>
+        {children}
+      </ProjectDispatchContext.Provider>
+    </ProjectStateContext.Provider>
+  )
+}
+
+const useProjectState = () => {
+  const context = React.useContext(ProjectStateContext)
+  if (context === undefined) {
+    throw new Error(`useProjectState must be used within a ProjectProvider`)
+  }
+  return context
+}
+
+const useProjectDispatch = () => {
+  const context = React.useContext(ProjectDispatchContext)
+  if (context === undefined) {
+    throw new Error(`useProjectDispatch must be used within a ProjectProvider`)
+  }
+  return context
+}
+
+export { ProjectProvider, useProjectState, useProjectDispatch }
