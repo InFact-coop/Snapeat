3207760fd679936f14407e8e3f1ab5df5a3a063c
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-12-03 12:07:36 +0000

    Add very basic styles to landing

--- apps/UnauthenticatedApp.js
+++ apps/UnauthenticatedApp.js
@@ -1,14 +1,29 @@
 import styled from 'styled-components'
 
+import logo from '../public/logos/logo1.svg'
+
+import { Button } from '../components/SecurityPages'
+
 const Container = styled.section.attrs({
-  className: 'bg-lightgray w-screen',
+  className:
+    'bg-lightgray w-screen h-screen flex flex-col justify-center items-center p-4',
+})``
+
+const Logo = styled.img.attrs({
+  src: logo,
+  className: 'm-6',
 })``
 
 const App = () => {
   return (
     <Container>
-      UnauthenticatedApp <a href="/api/login">Login</a>
-      <a href="/api/logout">Logout</a>
+      <Logo />
+      <Button className="m-6">
+        <a href="/api/login">Log in</a>
+      </Button>
+      <a className="underline m-6" href="/api/logout">
+        Or Sign up
+      </a>
     </Container>
   )
 }
