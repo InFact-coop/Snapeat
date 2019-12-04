6c5fc525050f8a3991ae7dca179fa52a58ed9b86
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-12-04 15:23:04 +0000

    Move 404 to next js _errors page

--- pages/_error.js
+++ pages/_error.js
@@ -4,9 +4,9 @@ import styled from 'styled-components'
 import logo from '../public/logos/logo1.svg'
 import robot from '../public/icons/404-robot.svg'
 
-const PageNotFound = () => {
+function Error({ statusCode }) {
   return (
-    <div className="flex flex-col justify-between items-center w-full h-full px-4 pt-6 pb-12">
+    <div className="flex flex-col justify-between items-center w-full h-screen px-4 pt-6 pb-12">
       <a href="/">
         <img src={logo} alt="Snapeat logo" />
       </a>
@@ -16,7 +16,9 @@ const PageNotFound = () => {
         alt="A cute little error robot"
       />
       <div>
-        <Text className="text-red font-bold">Oooops, it&apos;s a 404!</Text>
+        <Text className="text-red font-bold">
+          Oooops, it&apos;s {statusCode ? `a ${statusCode}` : 'an error'}!
+        </Text>
         <Text className="text-red">
           We noticed you lost your way. Not to worry though!
         </Text>
@@ -42,4 +44,9 @@ const Text = styled.p.attrs({
   className: 'text-xl text-center m-8',
 })``
 
-export default PageNotFound
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
+
+export default Error
