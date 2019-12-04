6115cdee1722a3368351897983ca1b204293846a
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-12-04 12:04:19 +0000

    Finish security pages

--- views/Security.js
+++ views/Security.js
@@ -5,8 +5,9 @@ import { useRouteDispatchUnauth } from '../context/unauthRouteContext'
 import {
   HOME,
   CHANGE_VIEW,
-  // TERMS_AND_CONDITIONS_AUTH,
+  TERMS_AND_CONDITIONS_AUTH,
   TERMS_AND_CONDITIONS_UNAUTH,
+  PRIVACY_AUTH,
   PRIVACY_UNAUTH,
   WELCOME,
 } from '../utils/constants'
@@ -23,73 +24,34 @@ import {
 
 export const SecurityAuth = () => {
   const routeDispatch = useRouteDispatch()
-
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
   return (
-    <div>
-      <HeaderWithGSTCLogo text="T&C & Privacy" />
-
-      <div className="py-1">
-        <Text>
-          Snapeat is owned and managed by Guys & St Thomas&#39; Charitable
-          Trust. <br />
-          Everything you share on Snapeat will be stored safely and your
-          personal data will always be private. Read our Plain English Privacy
-          Policy{' '}
-          <button
-            className="inline underline text-blue"
-            onClick={() =>
-              routeDispatch({ type: CHANGE_VIEW, view: PRIVACY_UNAUTH })
-            }
-          >
-            here
-          </button>
-          <span>.</span>
-        </Text>
-
-        <Text>
-          If you are still happy to take part in the Snapeat Project please read
-          our{' '}
-          <button
-            className="inline underline text-blue"
-            onClick={() =>
-              routeDispatch({
-                type: CHANGE_VIEW,
-                view: TERMS_AND_CONDITIONS_UNAUTH,
-              })
-            }
-          >
-            Terms and Conditions
-          </button>{' '}
-          and, if you agree with them, click &#39;I Agree&#39; so that your
-          family can take part.
-        </Text>
-      </div>
-      <Footer>
-        <Grid>
-          <Checkbox type="checkbox" id="termsConditions" />
-          <Label htmlFor="termsConditions">
-            I have read and agree to the terms and conditions and the privacy
-            policy.
-          </Label>
-        </Grid>
-        <Button
-          onClick={() => {
-            routeDispatch({ type: CHANGE_VIEW, view: HOME })
-          }}
-        >
-          I Agree
-        </Button>
-      </Footer>
-    </div>
+    <Content
+      routeDispatch={routeDispatch}
+      privacy={PRIVACY_AUTH}
+      termsAndConditions={TERMS_AND_CONDITIONS_AUTH}
+      continueTo={HOME}
+    />
   )
 }
 
 export const SecurityUnauth = () => {
   const routeDispatch = useRouteDispatchUnauth()
+  return (
+    <Content
+      routeDispatch={routeDispatch}
+      privacy={PRIVACY_UNAUTH}
+      termsAndConditions={TERMS_AND_CONDITIONS_UNAUTH}
+      continueTo={WELCOME}
+    />
+  )
+}
 
+const Content = ({
+  routeDispatch,
+  privacy,
+  termsAndConditions,
+  continueTo,
+}) => {
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
@@ -106,9 +68,7 @@ export const SecurityUnauth = () => {
           Policy{' '}
           <button
             className="inline underline text-blue"
-            onClick={() =>
-              routeDispatch({ type: CHANGE_VIEW, view: PRIVACY_UNAUTH })
-            }
+            onClick={() => routeDispatch({ type: CHANGE_VIEW, view: privacy })}
           >
             here
           </button>
@@ -123,7 +83,7 @@ export const SecurityUnauth = () => {
             onClick={() =>
               routeDispatch({
                 type: CHANGE_VIEW,
-                view: TERMS_AND_CONDITIONS_UNAUTH,
+                view: termsAndConditions,
               })
             }
           >
@@ -143,7 +103,7 @@ export const SecurityUnauth = () => {
         </Grid>
         <Button
           onClick={() => {
-            routeDispatch({ type: CHANGE_VIEW, view: WELCOME })
+            routeDispatch({ type: CHANGE_VIEW, view: continueTo })
           }}
         >
           I Agree
